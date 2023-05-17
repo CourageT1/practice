758c5e418a4d179f6f1230c0d8a6a42b107ac810
CourageT1 <124376251+CourageT1@users.noreply.github.com>
2023-05-17 20:48:52 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * main - function that checks if our shell is called
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	/* determines if file descriptor is associated with a terminal */
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		prompt();
+	}
+	else
+	{
+		non-prompt();
+	}
+	return (0);
+}
