77603da816ed6b04793ad7443c11a167fa4ae984
talent <124376251+CourageT1@users.noreply.github.com>
2023-05-18 21:41:52 +0200

    Update and rename read_line.c to scan_line.c

---
+++ scan_line.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * scan_line - Reads line from stdin.
+ *
+ * Return: Pointer to a string with the line content.
+ */
+char *read_line(void)
+{
+    char *line = NULL;
+    size_t bufsize = 0;
+
+    if (getline(&line, &bufsize, stdin) == -1)
+    {
+        if (feof(stdin))
+        {
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            free(line);
+            perror("Error while reading the line from stdin");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return (line);
+}
