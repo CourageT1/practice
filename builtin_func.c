c4e1165062c9651e4f9a25a3b0174d9425427a38
talent <124376251+CourageT1@users.noreply.github.com>
2023-05-21 16:25:45 +0200

    Update builtin_func.c

--- builtin_func.c
+++ builtin_func.c
@@ -28,7 +28,8 @@ int own_cd(char **argv)
 {
 if (argv[1] == NULL)
 {
-write(STDERR_FILENO, "expected argument to \"cd\"\n", strlen("expected argument to \"cd\"\n"));
+write(STDERR_FILENO, "expected argument to \"cd\"\n",
+      strlen("expected argument to \"cd\"\n"));
 }
 else
 {
@@ -70,30 +71,29 @@ return (-1);
  */
 int own_help(char **argv)
 {
-    char *builtin_func_list[] = {
+char *builtin_func_list[] = {
         "cd",
         "env",
         "help",
         "exit"
-    };
-    unsigned int i = 0;
-    (void)(**argv);
+};
+unsigned int i = 0;
+(void)(**argv);
 
-    write(STDOUT_FILENO, "\n---help simple_shell---\n",
-	  strlen("\n---help simple_shell---\n"));
-    write(STDOUT_FILENO, "Type a command and its arguments, then hit enter\n",
-	  strlen("Type a command and its arguments, then hit enter\n"));
-    write(STDOUT_FILENO, "Built-in commands:\n", strlen("Built-in commands:\n"));
+write(STDOUT_FILENO, "\n---help simple_shell---\n",
+      strlen("\n---help simple_shell---\n"));
+write(STDOUT_FILENO, "Type a command and its arguments, then hit enter\n",
+      strlen("Type a command and its arguments, then hit enter\n"));
+write(STDOUT_FILENO, "Built-in commands:\n",
+      strlen("Built-in commands:\n"));
 
-    for (; i < sizeof(builtin_func_list) / sizeof(char *); i++)
-    {
-        char buffer[32];
-        int n = snprintf(buffer, sizeof(buffer), "  -> %s\n", builtin_func_list[i]);
-        write(STDOUT_FILENO, buffer, n);
-    }
-
-    write(STDOUT_FILENO, "Use the man command for information on other programs.\n\n",
-          strlen("Use the man command for information on other programs.\n\n"));
-
+for (; i < sizeof(builtin_func_list) / sizeof(char *); i++)
+{
+char buffer[32];
+int n = snprintf(buffer, sizeof(buffer), "  -> %s\n", builtin_func_list[i]);
+write(STDOUT_FILENO, buffer, n);
+}
+write(STDOUT_FILENO, "Use the man command for information on other programs.\n\n",
+      strlen("Use the man command for information on other programs.\n\n"));
 return (-1);
 }
