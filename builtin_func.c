1e8b440f1be01126b4bb9fe6972089e00a0e9196
talent <124376251+CourageT1@users.noreply.github.com>
2023-05-21 16:11:42 +0200

    Update builtin_func.c

--- builtin_func.c
+++ builtin_func.c
@@ -60,32 +60,68 @@ i++;
 return (-1);
 }
 
+}
+#include "shell.h"
 /**
- * own_help - print help
- * @argv: arguments
- *
- * Return: 1 on success, 0 otherwise
- */
+* own_help - print help
+* @argv: arguments
+*
+* Return: 1 (success), 0 (otherwise)
+*/
 int own_help(char **argv)
 {
-char *builtin_func_list[] = {
-"cd",
-"env",
-"help",
-"exit"
-};
+char *builtin_func_list[] = {"cd", "env", "help", "exit"};
 unsigned int i = 0;
 (void)(**argv);
-
-write(STDOUT_FILENO, "\n---help simple_shell---\n", strlen("\n---help simple_shell---\n"));
-write(STDOUT_FILENO, "Type a command and its arguments, then hit enter\n", strlen("Type a command and its arguments, then hit enter\n"));
-write(STDOUT_FILENO, "Built-in commands:\n", strlen("Built-in commands:\n"));
-for (; i < sizeof(builtin_func_list) / sizeof(char *); i++)
+int fd = open("help.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+if (fd == -1)
 {
-char buffer[32];
-int n = snprintf(buffer, sizeof(buffer), "  -> %s\n", builtin_func_list[i]);
-write(STDOUT_FILENO, buffer, n);
+perror("open");
+return (0);
 }
-write(STDOUT_FILENO, "Use the man command for information on other programs.\n\n", strlen("Use the man command for information on other programs.\n\n"));
+const char *messages[] = {
+"---help simple_shell---",
+"Type a command and its arguments, then hit enter",
+"Built-in commands:"
+ };
+ssize_t n;
+for (i = 0; i < sizeof(messages) / sizeof(const char *); i++)
+{
+n = write(fd, messages[i], strlen(messages[i]));
+if (n == -1)
+{
+perror("write");
+close(fd);
+return (0);
+}
+}
+char buffer[32];
+ssize_t write_result;
+for (i = 0; i < sizeof(builtin_func_list) / sizeof(char *); i++)
+{
+n = snprintf(buffer, sizeof(buffer), "  -> %s\n", builtin_func_list[i]);
+if (n < 0 || n >= sizeof(buffer))
+{
+perror("snprintf");
+close(fd);
+return (0);
+}
+write_result = write(fd, buffer, n);
+if (write_result == -1)
+{
+perror("write");
+close(fd);
+return (0);
+}
+}
+const char *end_message = "Use the man command for information on other programs.\n\n";
+ssize_t end_write_result = write(fd, end_message, strlen(end_message));
+if (end_write_result == -1)
+{
+perror("write");
+close(fd);
+return (0);
+}
+close(fd);
 return (-1);
 }
