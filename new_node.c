64adb456cbfd5a496d3b317b420ccb49a481be4f
talent <124376251+CourageT1@users.noreply.github.com>
2023-05-22 16:32:28 +0200

    Update new_node.c

--- new_node.c
+++ new_node.c
@@ -8,32 +8,44 @@
  */
 int new_node(char **argv)
 {
-  pid_t pid;
-  int status;
-  const char *builtins[] = {
-    "cal",
-    "ls",
-    "help",
-    "exit",
-    "cd",
-    "env",
-    NULL
-    };
+pid_t pid;
+int status;
+
 pid = fork();
 if (pid == -1) {
-perror("Error in new_node: fork");
+write(STDERR_FILENO, "Error in new_node: fork\n", 24);
 return (0);
-} else if (pid == 0) {
-if (is_builtin(argv[0], builtins)) {
-execute_builtin(argv[0], argv);
-} else {
+}
+else if (pid == 0)
+{
+if (is_builtin(argv[0]))
+{
+const char *path = get_builtin_path(argv[0]);
+if (path != NULL)
+{
+execve(path, argv, NULL);
+}
+else
+{
+const char *message1 = "Unknown command: ";
+const char *message2 = "\n";
+write(STDERR_FILENO, message1, strlen(message1));
+write(STDERR_FILENO, argv[0], strlen(argv[0]));
+write(STDERR_FILENO, message2, strlen(message2));
+}
+}
+else
+{
 child_process(argv);
 }
-} else {
+_exit(EXIT_SUCCESS);
+}else
+{
 waitpid(pid, &status, WUNTRACED);
 }
 return (1);
 }
+
 /**
 * child_process - creates process to execute command
 * @argv: array to execute
@@ -42,7 +54,7 @@ return (1);
 */
 void child_process(char **argv)
 {
-execve(argv[0], argv);
-perror("execve");
-exit(EXIT_FAILURE);
+execve(argv[0], argv, NULL);
+write(STDERR_FILENO, "execve error\n", 13);
+_exit(EXIT_FAILURE);
 }
